cc71273336be1a5d25be5b6f6880612fd242af76
yzemlyak <yzemlyak@gmail.com>
2025-05-13 03:45:20 +0000

    Deleting actor added

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -18,6 +18,14 @@ class ActorsController < ApplicationController
     redirect_to("/actors")
   end 
 
+  def destroy 
+    the_id = params.fetch("an_id")    
+    matching_records = Actor.where({:id => the_id })
+    the_actor = matching_records.at(0)
+    the_actor.destroy
+    redirect_to("/actors")
+  end
+
   def show
     the_id = params.fetch("path_id")
 
