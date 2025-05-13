67c1b7648fdc1c9e0ce641fe3c0546c255fe6c08
yzemlyak <yzemlyak@gmail.com>
2025-05-13 01:52:16 +0000

    Added delete function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,15 @@ class MoviesController < ApplicationController
     redirect_to("/movies")
   end 
 
+  def destroy 
+    the_id = params.fetch("an_id")    
+
+    matching_records = Movie.where({:id => the_id })
+    the_movie = matching_records.at(0)
+    the_movie.destroy
+    redirect_to("/movies")
+  end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
