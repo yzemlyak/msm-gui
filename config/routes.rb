55781134c74f976720f089073e6164262c61c81a
yzemlyak <yzemlyak@gmail.com>
2025-05-13 05:00:04 +0000

    Deleting director

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
-
+  post("/insert_director_record", {:controller => "directors", :action => "create"})
+  get("/delete_director/:an_id", {:controller => "directors", :action => "destroy"})
+  
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   post("/insert_movie_record", {:controller => "movies", :action => "create"})
