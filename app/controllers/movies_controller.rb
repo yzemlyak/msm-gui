e33595ecb713135b06423b5d2d290be14d0b07a1
yzemlyak <yzemlyak@gmail.com>
2025-05-13 01:27:53 +0000

    More work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,18 @@
 class MoviesController < ApplicationController
+  def create 
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.duration = params.fetch("the_duration")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+  end 
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
