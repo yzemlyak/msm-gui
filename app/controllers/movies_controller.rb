43631407d062e47f25fa3769e4c4e7df932999b8
yzemlyak <yzemlyak@gmail.com>
2025-05-13 02:13:08 +0000

    added movies/edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,18 @@
 class MoviesController < ApplicationController
+  def update 
+    m_id = params.fetch("the_id")
+    matching_records = Movie.where({:id => m_id})
+    the_movie = matching_records.at(0)
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.director_id = params.fetch("the_director_id")
+    the_movie.save
+    redirect_to("/movies/#{the_movie.id}")
+  end
+  
   def create 
     m = Movie.new
     m.title = params.fetch("the_title")
